9ee9fc1a9ec5bb0387e100f7b102533095e6ecfa
Edwin Donovan <me@edwindonovan.com>
2023-04-30 22:30:46 -0400

    install script tweak

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ dirs=('dunst' 'hypr' 'kitty' 'swayidle' 'waybar' 'wlogout' 'wofi')
 
 for dir in "${dirs[@]}" ; do
   if [[ -e $XDG_CONFIG_HOME/$dir ]] ; then
-    mvdir $XDG_CONFIG_HOME/$dir "$XDG_CONFIG_HOME//${dir}.orig" && echo "Renamed existing $XDG_CONFIG_HOME//$dir to $XDG_CONFIG_HOME//${dir}.conf" 2>> error.txt
+    mvdir $XDG_CONFIG_HOME/$dir "$XDG_CONFIG_HOME/${dir}.orig" && echo "Renamed existing $XDG_CONFIG_HOME/$dir to $XDG_CONFIG_HOME/${dir}.conf"
   fi
-  cp -r ./.config/$dir $XDG_CONFIG_HOME/$dir && echo "Copied .//.config//$dir to $XDG_CONFIG_HOME//$dir" 2>> error.txt
+  cp -r ./.config/$dir $XDG_CONFIG_HOME/$dir && echo "Copied ./.config/$dir to $XDG_CONFIG_HOME/$dir"
 done
 
-read -p "Do you want to copy over the .bashrc too? y//n" cpbashrc
+read -p "Do you want to copy over the .bashrc too? y/n" cpbashrc
 case $cpbashrc in
   y | Y | yes)
     if [[ -f $HOME/.bashrc ]] ; then
-      mv $HOME/.bashrc $HOME/.bashrc_orig && echo "Renamed existing .bashrc to .bashrc_orig" 2>> error.txt
+      mv $HOME/.bashrc $HOME/.bashrc_orig && echo "Renamed existing .bashrc to .bashrc_orig" 
     fi
-    cp ./.bashrc $HOME/.bashrc && echo "Copied .bashrc" 2>> error.txt
+    cp ./.bashrc $HOME/.bashrc && echo "Copied .bashrc"
     ;;
   n | N | no)
     echo "Ok, not copying .bashrc"
