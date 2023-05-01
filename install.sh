21e79690d4615aab1e38e15dbb156e4163089dd3
Edwin Donovan <me@edwindonovan.com>
2023-04-30 22:24:14 -0400

    install script tweak

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 dirs=('dunst' 'hypr' 'kitty' 'swayidle' 'waybar' 'wlogout' 'wofi')
 
 for dir in "${dirs[@]}" ; do
-  if [[ -e $XDG_CONFIG_HOME/$dir ]] ; do
+  if [[ -e $XDG_CONFIG_HOME/$dir ]] ; then
     mvdir $XDG_CONFIG_HOME/$dir "$XDG_CONFIG_HOME//${dir}.orig" && echo "Renamed existing $XDG_CONFIG_HOME//$dir to $XDG_CONFIG_HOME//${dir}.conf" 2>> error.txt
   fi
   cp -r ./.config/$dir $XDG_CONFIG_HOME/$dir && echo "Copied .//.config//$dir to $XDG_CONFIG_HOME//$dir" 2>> error.txt
@@ -14,7 +14,7 @@ done
 read -p "Do you want to copy over the .bashrc too? y//n" cpbashrc
 case $cpbashrc in
   y | Y | yes)
-    if [[ -f $HOME/.bashrc ]] ; do
+    if [[ -f $HOME/.bashrc ]] ; then
       mv $HOME/.bashrc $HOME/.bashrc_orig && echo "Renamed existing .bashrc to .bashrc_orig" 2>> error.txt
     fi
     cp ./.bashrc $HOME/.bashrc && echo "Copied .bashrc" 2>> error.txt
